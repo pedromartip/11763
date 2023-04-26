b724882497723486a7a989952cb12f3f0f14bad5
PBibiloni <pedro@bibiloni.es>
2023-04-26 12:39:42 +0200

    Delete solutions

--- activity02.py
+++ activity02.py
@@ -21,9 +21,6 @@ def segment_bones(img_ct: np.ndarray) -> np.ndarray:
     #   should return a boolean mask (positive/negative) or an integer mask (labels)?
     #   See `skimage.measure.label(...)`.
     # ...
-    mask_bone = img_ct > 250    # Which is the best threshold?
-    mask_bone_labels = measure.label(mask_bone)
-    return mask_bone_labels
 
 
 def visualize_side_by_side(img: np.ndarray, mask: np.ndarray):
@@ -33,17 +30,12 @@ def visualize_side_by_side(img: np.ndarray, mask: np.ndarray):
     #   Which colormap should you choose?
     #   Which aspect ratio should you choose?
     # ...
-    plt.subplot(211), plt.imshow(img, cmap=matplotlib.colormaps['bone'], aspect=0.98/3.27)
-    plt.subplot(212), plt.imshow(mask, cmap=matplotlib.colormaps['prism'], aspect=0.98/3.27)
-    plt.show()
 
 
 def apply_cmap(img: np.ndarray, cmap_name: str = 'bone') -> np.ndarray:
     """ Apply a colormap to a 2D image. """
     # Your code here: See `matplotlib.colormaps[...]`.
     # ...
-    cmap_function = matplotlib.colormaps[cmap_name]
-    return cmap_function(img)
 
 
 def visualize_alpha_fusion(img: np.ndarray, mask: np.ndarray, alpha: float = 0.25):
@@ -52,14 +44,6 @@ def visualize_alpha_fusion(img: np.ndarray, mask: np.ndarray, alpha: float = 0.2
     #   Remember the Painter's Algorithm with alpha blending
     #   https://en.wikipedia.org/wiki/Alpha_compositing
     # ...
-    img_sagittal_cmapped = apply_cmap(img, cmap_name='bone')    # Why 'bone'?
-    mask_bone_cmapped = apply_cmap(mask, cmap_name='prism')     # Why 'prism'?
-    mask_bone_cmapped = mask_bone_cmapped * mask[..., np.newaxis].astype('bool')
-
-    alpha = 0.25
-    plt.imshow(img_sagittal_cmapped * (1-alpha) + mask_bone_cmapped * alpha, aspect=0.98/3.27)
-    plt.title(f'Segmentation with alpha {alpha}')
-    plt.show()
 
 
 if __name__ == '__main__':
