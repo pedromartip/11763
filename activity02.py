f3276af1d7e34ba4b63c1abb3486eedb16353c1e
PBibiloni <pedro@bibiloni.es>
2023-04-26 12:38:46 +0200

    General improvement

--- activity02.py
+++ activity02.py
@@ -1,7 +1,10 @@
+import matplotlib
 import pydicom
 import numpy as np
 from matplotlib import pyplot as plt
 from skimage import measure
+
+from utils import filepath
 
 
 def median_sagittal_plane(img_dcm: np.ndarray) -> np.ndarray:
@@ -24,34 +27,34 @@ def segment_bones(img_ct: np.ndarray) -> np.ndarray:
 
 
 def visualize_side_by_side(img: np.ndarray, mask: np.ndarray):
-    """ Visualize image and mask side by side. """
+    """ Visualize image and mask in two different subplots. """
     # Your code here:
     #   See `plt.subplot(...)`, `plt.imshow(...)`, `plt.show(...)`.
     #   Which colormap should you choose?
     #   Which aspect ratio should you choose?
     # ...
-    plt.subplot(211), plt.imshow(img, cmap=plt.cm.get_cmap('bone'), aspect=0.98/3.27)
-    plt.subplot(212), plt.imshow(mask, cmap=plt.cm.get_cmap('prism'), aspect=0.98/3.27)
+    plt.subplot(211), plt.imshow(img, cmap=matplotlib.colormaps['bone'], aspect=0.98/3.27)
+    plt.subplot(212), plt.imshow(mask, cmap=matplotlib.colormaps['prism'], aspect=0.98/3.27)
     plt.show()
 
 
 def apply_cmap(img: np.ndarray, cmap_name: str = 'bone') -> np.ndarray:
     """ Apply a colormap to a 2D image. """
-    # Your code here: See `plt.cm.get_cmap(...)`.
+    # Your code here: See `matplotlib.colormaps[...]`.
     # ...
-    cmap_function = plt.cm.get_cmap(cmap_name)
+    cmap_function = matplotlib.colormaps[cmap_name]
     return cmap_function(img)
 
 
 def visualize_alpha_fusion(img: np.ndarray, mask: np.ndarray, alpha: float = 0.25):
-    """ Visualize image and mask with alpha fusion. """
+    """ Visualize both image and mask in the same plot. """
     # Your code here:
     #   Remember the Painter's Algorithm with alpha blending
     #   https://en.wikipedia.org/wiki/Alpha_compositing
     # ...
     img_sagittal_cmapped = apply_cmap(img, cmap_name='bone')    # Why 'bone'?
     mask_bone_cmapped = apply_cmap(mask, cmap_name='prism')     # Why 'prism'?
-    mask_bone_cmapped = mask_bone_cmapped * mask[..., np.newaxis]
+    mask_bone_cmapped = mask_bone_cmapped * mask[..., np.newaxis].astype('bool')
 
     alpha = 0.25
     plt.imshow(img_sagittal_cmapped * (1-alpha) + mask_bone_cmapped * alpha, aspect=0.98/3.27)
@@ -60,12 +63,13 @@ def visualize_alpha_fusion(img: np.ndarray, mask: np.ndarray, alpha: float = 0.2
 
 
 if __name__ == '__main__':
-    dcm = pydicom.dcmread('16351644_s1_CT_PETCT.dcm')   # Load DICOM file
-    img_dcm = dcm.pixel_array                           # Get pixel array
-    str(dcm)                                            # Print DICOM headers
+    dcm_path = filepath('CT.dcm')
+    dcm = pydicom.dcmread(dcm_path)   # Load DICOM file
+    img_dcm = dcm.pixel_array         # Get pixel array
+    print(dcm)                          # Print DICOM headers
 
     img_sagittal = median_sagittal_plane(img_dcm)
-    mask_bone = segment_bones(img_dcm)
+    mask_bone = segment_bones(img_sagittal)
 
     visualize_side_by_side(img_sagittal, mask_bone)
     visualize_alpha_fusion(img_sagittal, mask_bone)
