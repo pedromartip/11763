2a8eb9e040e764fcdbefacd87cfc23cf5ac44070
PBibiloni <pedro@bibiloni.es>
2023-05-10 12:00:46 +0200

    Update expected values

--- utils.py
+++ utils.py
@@ -1,9 +1,15 @@
 import os
+from typing import Sequence
 
 
 def filepath(filename, subfolder='data'):
     folder = os.path.dirname(__file__)
     return f'{folder}/{subfolder}/{filename}'
+
+
+def str_floats(tuple: Sequence[float]) -> str:
+    return f'({", ".join([f"{coord:0.02f}" for coord in tuple])})'
+
 
 def str_quaternion(q: tuple[float, float, float, float]) -> str:
     return f'{q[0]} + {q[1]}i + {q[2]}j + {q[3]}k'
