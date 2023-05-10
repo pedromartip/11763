08bda2599c5dbde92fef5467d65f8c291e89c515
PBibiloni <pedro@bibiloni.es>
2023-05-10 19:07:53 +0200

    Update str formatting utils

--- utils.py
+++ utils.py
@@ -8,12 +8,12 @@ def filepath(filename, subfolder='data'):
 
 
 def str_floats(sequence_of_floats: Sequence[float]) -> str:
-    if not sequence_of_floats:
+    if sequence_of_floats is None:
         return '(None)'
     return f'({", ".join([f"{coord:0.02f}" for coord in sequence_of_floats])})'
 
 
 def str_quaternion(q: tuple[float, float, float, float]) -> str:
-    if not q:
+    if q is None:
         return '[None]'
     return f'{q[0]} + {q[1]}i + {q[2]}j + {q[3]}k'
