2952f40352c0630702f4e30eb17561ef3c1a93fd
PBibiloni <pedro@bibiloni.es>
2023-05-10 12:05:20 +0200

    Update expected values

--- utils.py
+++ utils.py
@@ -7,13 +7,13 @@ def filepath(filename, subfolder='data'):
     return f'{folder}/{subfolder}/{filename}'
 
 
-def str_floats(tuple: Sequence[float]) -> str:
-    if not tuple:
+def str_floats(sequence_of_floats: Sequence[float]) -> str:
+    if not sequence_of_floats:
         return '(None)'
-    return f'({", ".join([f"{coord:0.02f}" for coord in tuple])})'
+    return f'({", ".join([f"{coord:0.02f}" for coord in sequence_of_floats])})'
 
 
 def str_quaternion(q: tuple[float, float, float, float]) -> str:
-    if not tuple:
+    if not q:
         return '[None]'
     return f'{q[0]} + {q[1]}i + {q[2]}j + {q[3]}k'
