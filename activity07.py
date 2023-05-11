e28037ebd8306d723385114d047844e3bc8161bd
PBibiloni <pedro@bibiloni.es>
2023-05-11 11:37:47 +0200

    Delete solutions

--- activity07.py
+++ activity07.py
@@ -8,23 +8,12 @@ from skimage.morphology import binary_dilation, binary_erosion
 def get_amygdala_mask(img_atlas: np.ndarray) -> np.ndarray:
     # Your code here:
     #   ...
-    amygdala_mask = np.zeros_like(img_atlas)
-    amygdala_mask[img_atlas == 45] = 1
-    amygdala_mask[img_atlas == 46] = 1
-    return amygdala_mask
 
 
 def find_centroid(mask: np.ndarray) -> np.ndarray:
     # Your code here:
     #   Consider using `np.where` to find the indices of the voxels in the mask
     #   ...
-    idcs = np.where(mask == 1)
-    centroid = np.stack([
-        np.mean(idcs[0]),
-        np.mean(idcs[1]),
-        np.mean(idcs[2]),
-    ])
-    return centroid
 
 
 def visualize_axial_slice(
@@ -37,23 +26,12 @@ def visualize_axial_slice(
     #   Remember `matplotlib.colormaps['cmap_name'](...)`
     #   See also `matplotlib.colors.Normalize(vmin=..., vmax=...)`
     #   ...
-    img_slice = img[mask_centroid[0].astype('int'), :, :]
-    mask_slice = mask[mask_centroid[0].astype('int'), :, :]
-
-    cmap = matplotlib.colormaps['bone']
-    norm = matplotlib.colors.Normalize(vmin=np.amin(img_slice), vmax=np.amax(img_slice))
-    fused_slice = \
-        0.5*cmap(norm(img_slice))[..., :3] + \
-        0.5*np.stack([mask_slice, np.zeros_like(mask_slice), np.zeros_like(mask_slice)], axis=-1)
-    plt.imshow(fused_slice)
-    plt.show()
 
 
 def find_region_volume(region_mask):
     """ Returns the volume of the region in mm^3. """
     # Your code here:
     #   ...
-    return np.sum(region_mask)
 
 
 def find_region_surface(mask):
@@ -61,9 +39,6 @@ def find_region_surface(mask):
     # Your code here:
     #   See `skimage.morphology.binary_erosion()` and `skimage.morphology.binary_dilation()`
     #   ...
-    inner_surface = mask - binary_erosion(mask, np.ones((3, 3, 3)))
-    outer_surface = binary_dilation(mask, np.ones((3, 3, 3))) - mask
-    return (np.sum(inner_surface) + np.sum(outer_surface) ) / 2     # Average of inner and outer surface
 
 
 if __name__ == '__main__':
