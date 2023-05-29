aaaaf512fabd9ff4eb01d77a4b251e44a7d19a8a
PBibiloni <pedro@bibiloni.es>
2023-05-29 12:11:25 +0200

    Remove solutions

--- activity10.py
+++ activity10.py
@@ -22,7 +22,7 @@ def interpret_test(
     # YOUR CODE HERE:
     #   Set the value of `reject_null_hypothesis` to True or False, depending on the p-value and the significance level.
     # ...
-    reject_null_hypothesis = test_result.pvalue < significance_level
+    reject_null_hypothesis = False  # You might delete this line
 
     if reject_null_hypothesis:
         print(f'    >> We REJECT that "{null_hypothesis}".')
@@ -58,17 +58,6 @@ if __name__ == "__main__":
     #    Choose the most appropriate test.
     #    Calculate the test, and interpret the result.
     # ...
-    res_fsiq_piq_different = wilcoxon(
-        dataset['PIQ'],
-        dataset['FSIQ'],
-        alternative='two-sided'
-    )
-    interpret_test(
-        test_result=res_fsiq_piq_different,
-        significance_level=0.10,
-        null_hypothesis="FSIQ values FOLLOW a normal distribution",
-        altv_hypothesis="FSIQ values DO NOT FOLLOW a normal distribution"
-    )
 
     # 3) Determine wether a male's brain is significatively bigger in volume than a female's brain.
     # YOUR CODE HERE:
@@ -76,29 +65,6 @@ if __name__ == "__main__":
     #    Choose the most appropriate test.
     #    Calculate the test, and interpret the result.
     # ...
-    interpret_test(
-        test_result=shapiro(dataset[dataset['Gender'] == 'Female']['MRI_Count']),
-        significance_level=0.10,
-        null_hypothesis='Female MRI_Count values FOLLOW a normal distribution',
-        altv_hypothesis='Female MRI_Count values DO NOT follow a normal distribution',
-    )
-    interpret_test(
-        test_result=shapiro(dataset[dataset['Gender'] == 'Male']['MRI_Count']),
-        significance_level=0.10,
-        null_hypothesis='Male MRI_Count values FOLLOW a normal distribution',
-        altv_hypothesis='Male MRI_Count values DO NOT follow a normal distribution',
-    )
-    res_male_mricount_higher = wilcoxon(
-        dataset[dataset['Gender'] == 'Male']['MRI_Count'],
-        dataset[dataset['Gender'] == 'Female']['MRI_Count'],
-        alternative='greater'
-    )
-    interpret_test(
-        test_result=res_male_mricount_higher,
-        significance_level=0.10,
-        null_hypothesis='Median male MRI_Count is LOWER/EQUAL than median female MRI_Count',
-        altv_hypothesis='Median male MRI_Count is HIGHER than median female MRI_Count',
-    )
 
     # 4) Determine wether a male's IQ and a female's IQ are significantly different
     # YOUR CODE HERE:
@@ -106,15 +72,3 @@ if __name__ == "__main__":
     #    Choose the most appropriate test.
     #    Calculate the test, and interpret the result.
     # ...
-    for iq in ['PIQ', 'VIQ', 'FSIQ']:
-        res = wilcoxon(
-            dataset[dataset['Gender'] == 'Male'][iq],
-            dataset[dataset['Gender'] == 'Female'][iq],
-            alternative='two-sided'
-        )
-        interpret_test(
-            test_result=res,
-            significance_level=0.05,
-            null_hypothesis=f'Median male {iq} is EQUAL to median female {iq}',
-            altv_hypothesis=f'Median male {iq} is DIFFERENT than median female {iq}',
-        )
