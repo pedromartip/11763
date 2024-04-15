d5111cc28a2dd22a2e8e0c76a38ae71a3af41ef3
Pedro Marti <47566794+pedromartip@users.noreply.github.com>
2024-04-15 16:03:57 +0200

    Update activity05.py

--- activity05.py
+++ activity05.py
@@ -7,20 +7,24 @@ from utils import str_quaternion, str_floats
 
 
 def multiply_quaternions(
+                # real,   Ii,    Ij,     Ik
         q1: tuple[float, float, float, float],
         q2: tuple[float, float, float, float]
         ) -> tuple[float, float, float, float]:
     """ Multiply two quaternions, expressed as (1, i, j, k). """
-    # Your code here:
-    #   ...
+    return {
+        q1[0]*q2[0]-q1[1]*q2[1]-q1[2]*q2[2]-q1[3]*q2[3],
+        q1[0]*q2[1]+q1[1]*q2[2]+q1[2]*q2[3]-q1[3]*q2[0], # q1[1]*q2[2] --> Cambiar esto
+        q1[0]*q2[2]-q1[1]*q2[3]+q1[2]*q2[0]+q1[3]*q2[1],
+        q1[0]*q2[3]+q1[1]*q2[0]-q1[2]*q2[1]+q1[3]*q2[2],
+    }
 
 
 def conjugate_quaternion(
         q: tuple[float, float, float, float]
         ) -> tuple[float, float, float, float]:
     """ Multiply two quaternions, expressed as (1, i, j, k). """
-    # Your code here:
-    #   ...
+    
 
 
 def translation(
